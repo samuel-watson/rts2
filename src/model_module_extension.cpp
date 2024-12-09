037883305cab3fac3f46b70a180089c42d1bcddb
Samuel Watson (Applied Health Sciences) <s.i.watson@bham.ac.uk>
2024-12-09 10:14:37 +0000

    0.7.7 glmmr 11 version fix

--- src/model_module_extension.cpp
+++ src/model_module_extension.cpp
@@ -328,7 +328,13 @@ SEXP rtsModel__infomat_theta(SEXP xp, int covtype_, int lptype_){
   TypeSelector model(xp,covtype_,lptype_);
   auto functor = overloaded {
     [](int) { return returns(0);}, 
-    [](auto mptr){return returns(mptr->matrix.information_matrix_theta());}
+    [](auto mptr){
+#ifdef GLMMR11
+      return returns(mptr->matrix.template information_matrix_theta<glmmr::IM::EIM>());
+#else
+      return returns(mptr->matrix.information_matrix_theta());
+#endif
+    }
   };
   auto S = std::visit(functor,model.ptr);
   return wrap(std::get<Eigen::MatrixXd>(S));
